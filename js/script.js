7796524d0c54a24312d72bb6bfe3bd9b7a5c6333
AleksandrBida <aleksandr13b@gmail.com>
2023-12-26 23:53:32 +0200

    adaptiv33

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $('.burger').click(function () {
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
-
+    $('.bg-menu').toggleClass('active');
 })
 
 $('.close').click(function () {
     $('.nav').removeClass('active');
-    // $('.bg-menu').toggleClassClass('active');
+    $('.bg-menu').removeClass('active');
+
 }) 
